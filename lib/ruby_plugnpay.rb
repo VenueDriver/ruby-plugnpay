ad51ab3b315d93fd00194dc2f59625f539eeaf52
Ryan Alyn Porter <rap@endymion.com>
2009-07-11 13:37:47 -0400

    Added errors for badcard, problem.  Added unit test.  Added rdoc.

--- lib/ruby_plugnpay.rb
+++ lib/ruby_plugnpay.rb
@@ -1,53 +1,77 @@
+require 'cgi'
 require 'httparty'
 
 module PlugNPay
+
+  # A Ruby class for remote client use of the PlugNPay HTTP API.
   class Service
 
     include HTTParty
     format :html
     
-    attr_accessor :api_url,
-      :publisher_name
+    attr_accessor :api_url, :publisher_name, :debug
 
+    # Performs a merchant authorization using the PlugNPay HTTP API.
+    # 
+    # Example:
+    #   PlugNPay::Service.new('publisher-name'=>'youraccount').
     def initialize(params={})
       self.api_url = 'https://pay1.plugnpay.com/payment/pnpremote.cgi'
-      self.publisher_name = params['publisher-name'] unless params['publisher-name'].blank?
+      self.publisher_name = params['publisher-name'] if params['publisher-name']
+      self.debug = params['debug'] if params['debug']
     end
   
+    # Performs a merchant authorization using the PlugNPay HTTP API.
+    # 
     # Example:
-    #   PlugNPay::Service.new('publisher-name'=>'venuedrive').authorize('card-number' => '4111111111111111',
-    #   'card-name' => 'cardtest', 'card-name' => 'Visa', 'card-amount' => '1', 'card-exp' => '01/10')
-  
+    #   PlugNPay::Service.new('publisher-name'=>'youraccount').
+    #     authorize('card-number' => '4111111111111111',
+    #       'card-name' => 'cardtest', 'card-name' => 'Visa',
+    #       'card-amount' => '1', 'card-exp' => '01/10')  
     def authorize(params)
-      query = {}
-      query['card-number'] = params['card-number'] if params['card-number']
-      query['card-name'] = params['card-name'] if params['card-name']
-      query['card-type'] = params['card-name'] if params['card-type']
-      query['card-amount'] = params['card-amount'] if params['card-amount']
-      query['card-exp'] = params['card-exp'] if params['card-exp']
-      query['card-cvv'] = params['card-cvv'] if params['card-cvv']
-      query['email'] = params['email'] if params['email']
-      query['ship-name'] = params['ship-name'] if params['ship-name']
-      query['address1'] = params['address1'] if params['address1']
-      query['address2'] = params['address2'] if params['address2']
-      query['city'] = params['city'] if params['city']
-      query['state'] = params['state'] if params['state']
-      query['zip'] = params['zip'] if params['zip']
-      query['country'] = params['country'] if params['country']
 
-      query['publisher-name'] = publisher_name
-      query['mode'] = 'auth'
-      # arguments = arguments.map {|argument| ERB::Util::h(argument)}
+      query = {
+        'publisher-name' => publisher_name,
+        'mode' => 'auth'
+      }
 
-      require 'cgi'
-      puts url_unescape(Service.post(api_url, :query => query).gsub!(/\&/,"\n"))
-    end
+      # Set up HTTP request query parameters from method parameters.
+      possible_params = %w(card-number card-name card-type card-amount card-exp card-cvv
+        email ship-name address1 address2 city state zip country)
+      possible_params.each do |param|
+        query[param] = CGI.unescape(params[param]) if params[param]
+      end
 
-    def url_unescape(string)
-      string.tr('+', ' ').gsub(/((?:%[0-9a-fA-F]{2})+)/n) do
-        [$1.delete('%')].pack('H*')
+      # Perform HTTP request.
+      response_string = Service.post(api_url, :query => query)
+      puts CGI.unescape(response_string.gsub(/\&/,"\n")) if debug
+      
+      # Parse HTTP response into a Ruby hash.
+      response_values = response_string.split(/\&/)
+      response = Hash[*response_values.collect {|value|
+        [value.match(/^(.*)\=/)[1], value.match(/\=(.*)$/)[1]] }.flatten]
+      
+      # Raise errors if necessary.
+      case response['FinalStatus']
+        when /badcard/i
+          raise BadCard
+        when /problem/i
+          raise Problem
+        else
+          # Return the response.
+          response
       end
     end
 
   end
+
+  # Authorizations raise this error when the FinalStatus from PlugNPay is "badcard"
+  class BadCard < StandardError; end
+
+  # Authorizations raise this error when the FinalStatus from PlugNPay is "problem"
+  class Problem < StandardError; end
+
+  # Authorizations raise this error when the FinalStatus from PlugNPay is "fraud"
+  class Fraud < StandardError; end
+  
 end
--- test/ruby_plugnpay_test.rb
+++ test/ruby_plugnpay_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
+require 'ruby_plugnpay'
 
-class Ruby-plugnpayTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class Ruby_PlugNPayTest < ActiveSupport::TestCase
+
+  # Set your PlugNPay account name or the tests will not work.
+  publisher_name = 'youraccount'
+
+  test "authorization" do
+    assert PlugNPay::Service.new( 'publisher-name' => publisher_name ).
+      authorize(
+        'card-number' => '4111111111111111',
+        'card-name' => 'cardtest',
+        'card-type' => 'Visa',
+        'card-amount' => '1',
+        'card-exp' => '01/10')
   end
+
+  test "authorization with bad card" do
+    response = nil
+    badcard = false
+    begin
+      response = PlugNPay::Service.new( 'publisher-name' => publisher_name ).
+        authorize(
+          'card-number' => '4111111111111111',
+          'card-name' => 'cardtest',
+          'card-type' => 'Visa',
+          'card-amount' => '1500', # This amount causes a 'badcard' error.
+          'card-exp' => '01/10')
+    rescue PlugNPay::BadCard
+      badcard = true
+    end
+    assert response.nil?
+    assert badcard
+  end
+
+  test "authorization with problem" do
+    response = nil
+    problem = false
+    begin
+      response = PlugNPay::Service.new( 'publisher-name' => publisher_name ).
+        authorize(
+          'card-number' => '4111111111111111',
+          'card-name' => 'cardtest',
+          'card-type' => 'Visa',
+          'card-amount' => '2500', # This amount causes a 'problem' error.
+          'card-exp' => '01/10')
+    rescue PlugNPay::Problem
+      problem = true
+    end
+    assert response.nil?
+    assert problem
+  end
+
 end
